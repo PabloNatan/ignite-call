68acbe8ecfcf6f2876a2b21b558c9847b5f7c991
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2023-02-26 06:45:40 -0300

    move styles folder to src directory

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import { Roboto } from 'next/font/google'
-import { globalStyles } from './styles/global'
+import { globalStyles } from '../styles/global'
 
 const roboto = Roboto({ weight: ['400', '500', '700'], subsets: ['latin'] })
 
