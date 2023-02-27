b2c77221ddcd815fab7cd4deda7b6efa1aef977f
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2023-02-27 09:45:10 -0300

    configure next auth

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,15 +1,21 @@
 import type { AppProps } from 'next/app'
 import { Roboto } from 'next/font/google'
+import { SessionProvider } from 'next-auth/react'
 import { globalStyles } from '../styles/global'
 
 const roboto = Roboto({ weight: ['400', '500', '700'], subsets: ['latin'] })
 
 globalStyles()
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <div className={roboto.className}>
-      <Component {...pageProps} />
-    </div>
+    <SessionProvider session={session}>
+      <div className={roboto.className}>
+        <Component {...pageProps} />
+      </div>
+    </SessionProvider>
   )
 }
