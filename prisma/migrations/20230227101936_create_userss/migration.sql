12296779e2ce709d93d802e12f99a79744ad8986
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2023-02-27 07:22:52 -0300

    add prisma

---
+++ prisma/migrations/20230227101936_create_userss/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_name_key" ON "users"("name");
